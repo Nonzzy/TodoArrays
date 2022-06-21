c92ca5b4494f0e17973b31bd7e8c17cc5111c04a
unknown <nonzzym0@gmail.comgit config --global user.name>
2022-06-21 02:19:40 -0700

    my todo

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+
+
+function Navbar() {
+    return(
+
+<div class="sidebar">
+  <a href="#home"> Home</a>
+  <a href="#services"> Services</a>
+  <a href="#clients"> Clients</a>
+  <a href="#contact"> Contact</a>
+</div>
+    )
+}
+
+export default Navbar;
